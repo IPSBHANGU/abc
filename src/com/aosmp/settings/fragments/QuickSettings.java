f9fcaace24062e45885b1d958be7547ef3814a35
Roman Birg <roman@cyngn.com>
2015-11-11 14:59:28 -0500

    AOSmPSettings: Add Quick Settings Pulldown options
    Change-Id: Iec83668b0fa1bbb5b1d68e5091400ec20c798bac
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: Sebastian <kjjjnob.seba@gmail.com>

--- src/com/aosmp/settings/fragments/QuickSettings.java
+++ src/com/aosmp/settings/fragments/QuickSettings.java
@@ -16,54 +16,74 @@
 
 package com.aosmp.settings.fragments;
 
-import com.android.internal.logging.nano.MetricsProto;
-
 import android.os.Bundle;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.content.ContentResolver;
 import android.content.res.Resources;
+import android.support.v4.app.Fragment;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
-import android.support.v14.preference.SwitchPreference;
-import android.provider.Settings;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import java.util.Locale;
-import android.text.TextUtils;
-import android.view.View;
-
-import java.util.List;
-import java.util.ArrayList;
-
-public class QuickSettings extends SettingsPreferenceFragment implements
-        OnPreferenceChangeListener {
+import com.android.internal.logging.nano.MetricsProto;
 
 
-    @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+ public class QuickSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener{
 
-        addPreferencesFromResource(R.xml.aosmp_settings_quicksettings);
+ 	private static final String QUICK_PULLDOWN = "quick_pulldown";
 
-        PreferenceScreen prefScreen = getPreferenceScreen();
-        ContentResolver resolver = getActivity().getContentResolver();
+ 	private ListPreference mQuickPulldown;
 
-        }
+ 	@Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        addPreferencesFromResource(R.xml.qs_settings);        
+		ContentResolver resolver = getActivity().getContentResolver();
+        mQuickPulldown = (ListPreference) findPreference(QUICK_PULLDOWN);
+        mQuickPulldown.setOnPreferenceChangeListener(this);
+        int quickPulldownValue = Settings.System.getIntForUser(resolver,
+                Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 0, UserHandle.USER_CURRENT);
+        mQuickPulldown.setValue(String.valueOf(quickPulldownValue));
+        updatePulldownSummary(quickPulldownValue);
+    }
 
-    @Override
+     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-
+    	ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mQuickPulldown) {
+            int quickPulldownValue = Integer.valueOf((String) newValue);
+            Settings.System.putIntForUser(resolver, Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN,
+                    quickPulldownValue, UserHandle.USER_CURRENT);
+            updatePulldownSummary(quickPulldownValue);
+            return true;
+        }
         return false;
     }
 
-    @Override
+     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.AOSMP;
     }
 
-}
+     private void updatePulldownSummary(int value) {
+        Resources res = getResources();
+         if (value == 0) {
+            // quick pulldown deactivated
+            mQuickPulldown.setSummary(res.getString(R.string.quick_pulldown_off));
+        } else if (value == 3) {
+            // quick pulldown always
+            mQuickPulldown.setSummary(res.getString(R.string.quick_pulldown_summary_always));
+        } else {
+            String direction = res.getString(value == 2
+                    ? R.string.quick_pulldown_left
+                    : R.string.quick_pulldown_right);
+            mQuickPulldown.setSummary(res.getString(R.string.quick_pulldown_summary, direction));
+        }
+    }
+} 
+
