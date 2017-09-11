6a71f937d607e674b52eac213275a3991b3b3d0d
ezio84 <brabus84@gmail.com>
2017-09-11 06:46:31 +0530

    AOSmPSettings: Battery light customization [2/2]
    aex edits: Add back disable battery light toggle
    for obvious reason, also set other options dependencies
    on it
    
    Change-Id: I792c6428a774fb92b981e4e248d024c45acd6f4e
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: Sebastian <kjjjnob.seba@gmail.com>

--- src/com/aosmp/settings/fragments/NotificationSettings.java
+++ src/com/aosmp/settings/fragments/NotificationSettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 The AOSmP
+ * Copyright (C) 2019 AOSmP
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,24 +16,76 @@
  
 package com.aosmp.settings.fragments;
 
-import com.android.internal.logging.nano.MetricsProto;
-
+import android.app.ActivityManagerNative;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.WindowManagerGlobal;
+import android.view.IWindowManager;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import java.util.Locale;
+import android.text.TextUtils;
+import android.view.View;
+
 import com.android.settings.R;
-
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.aosmp.settings.preferences.Utils;
 
-public class NotificationSettings extends SettingsPreferenceFragment {
+ public class NotificationSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+
+    private PreferenceCategory mLedsCategory;
+    private Preference mChargingLeds;
 
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-        addPreferencesFromResource(R.xml.aosmp_settings_notifications);
+         addPreferencesFromResource(R.xml.aosmp_settings_notifications);
+
+        final ContentResolver resolver = getActivity().getContentResolver();
+        final PreferenceScreen prefSet = getPreferenceScreen();
+
+        mLedsCategory = (PreferenceCategory) findPreference("light_category");
+        mChargingLeds = (Preference) findPreference("battery_charging_light");
+        if (mChargingLeds != null
+                && !getResources().getBoolean(
+                        com.android.internal.R.bool.config_intrusiveBatteryLed)) {
+            mLedsCategory.removePreference(mChargingLeds);
+        }
+          if (mChargingLeds == null) {
+            prefSet.removePreference(mLedsCategory);
+        }
+
     }
 
-    @Override
+     @Override
     public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.AOSMP;
+        return MetricsEvent.AOSMP;
+    }
+
+     @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+     @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        return false;
     }
 }
