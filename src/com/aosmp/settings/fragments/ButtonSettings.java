a9094ac902db60ad18fe56a5fe61a0c14944bc50
Konsta <konsta09@gmail.com>
2017-09-30 11:25:42 +0530

    AOSmPSettings: Volume rocker cursor control [2/2]
    Change-Id: I4ece9684414f61eee849025a918f74771fd58972
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: Sebastian <kjjjnob.seba@gmail.com>

--- src/com/aosmp/settings/fragments/ButtonSettings.java
+++ src/com/aosmp/settings/fragments/ButtonSettings.java
@@ -34,31 +34,54 @@ import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
 
 import com.android.settings.R;
-
 import com.android.settings.SettingsPreferenceFragment;
-
 import com.android.internal.logging.nano.MetricsProto;
+import com.aosmp.settings.preferences.CustomSeekBarPreference;
 
-public class ButtonSettings extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener{
+	public class ButtonSettings extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener{
+    
+    private static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
+    private ListPreference mVolumeKeyCursorControl;    
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.aosmp_settings_button);
-
         final PreferenceScreen prefScreen = getPreferenceScreen();
+		ContentResolver resolver = getActivity().getContentResolver();
+		
+        // volume key cursor control
+        mVolumeKeyCursorControl = (ListPreference) findPreference(VOLUME_KEY_CURSOR_CONTROL);
+        if (mVolumeKeyCursorControl != null) {
+            mVolumeKeyCursorControl.setOnPreferenceChangeListener(this);
+            int volumeRockerCursorControl = Settings.System.getInt(getContentResolver(),
+                    Settings.System.VOLUME_KEY_CURSOR_CONTROL, 0);
+            mVolumeKeyCursorControl.setValue(Integer.toString(volumeRockerCursorControl));
+           mVolumeKeyCursorControl.setSummary(mVolumeKeyCursorControl.getEntry());
+        }
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-
-        return false;
+        if (preference == mVolumeKeyCursorControl) {
+            String volumeKeyCursorControl = (String) objValue;
+            int volumeKeyCursorControlValue = Integer.parseInt(volumeKeyCursorControl);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.VOLUME_KEY_CURSOR_CONTROL, volumeKeyCursorControlValue);
+            int volumeKeyCursorControlIndex = mVolumeKeyCursorControl
+                    .findIndexOfValue(volumeKeyCursorControl);
+            mVolumeKeyCursorControl
+                    .setSummary(mVolumeKeyCursorControl.getEntries()[volumeKeyCursorControlIndex]);
+            return true;                   
+        } else {
+            return false;
+		}
+        return true;
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.AOSMP;
     }
-
 }
