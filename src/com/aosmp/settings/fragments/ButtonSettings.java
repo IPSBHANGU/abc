1a15e2965a8551f808499606a58f1d073f5ac741
Sebastian <kjjjnob.seba@gmail.com>
2019-08-09 00:33:53 -0300

    Revert "AOSmPSettings: HW Keys customization support [2/2]"
    This reverts commit 53e21353660d6b4cb791b118ff9f5fa8b7ef9168.
    
    Signed-off-by: Sebastian <kjjjnob.seba@gmail.com>

--- src/com/aosmp/settings/fragments/ButtonSettings.java
+++ src/com/aosmp/settings/fragments/ButtonSettings.java
@@ -35,50 +35,21 @@ import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-
-import com.android.internal.util.hwkeys.ActionConstants;
-import com.android.internal.util.hwkeys.ActionUtils;
-import com.aosmp.settings.preferences.ActionFragment;
-
 import com.android.internal.logging.nano.MetricsProto;
 import com.aosmp.settings.preferences.CustomSeekBarPreference;
 
-	public class ButtonSettings extends ActionFragment implements OnPreferenceChangeListener { 
+	public class ButtonSettings extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener{
     
     private static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
-    private static final String HWKEY_DISABLE = "hardware_keys_disable";
 
-    // category keys
-    private static final String CATEGORY_HWKEY = "hardware_keys";
-    private static final String CATEGORY_HOME = "home_key";
-    private static final String CATEGORY_MENU = "menu_key";
-    private static final String CATEGORY_BACK = "back_key";
-    private static final String CATEGORY_ASSIST = "assist_key";
-    private static final String CATEGORY_APPSWITCH = "app_switch_key";
-
-    // Masks for checking presence of hardware keys.
-    // Must match values in frameworks/base/core/res/res/values/config.xml
-    // Masks for checking presence of hardware keys.
-    // Must match values in frameworks/base/core/res/res/values/config.xml
-    public static final int KEY_MASK_HOME = 0x01;
-    public static final int KEY_MASK_BACK = 0x02;
-    public static final int KEY_MASK_MENU = 0x04;
-    public static final int KEY_MASK_ASSIST = 0x08;
-    public static final int KEY_MASK_APP_SWITCH = 0x10;
-    public static final int KEY_MASK_CAMERA = 0x20;
-    public static final int KEY_MASK_VOLUME = 0x40;
-
-    private SwitchPreference mHwKeyDisable;
     private ListPreference mVolumeKeyCursorControl;    
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.aosmp_settings_button);
-
         final PreferenceScreen prefScreen = getPreferenceScreen();
-        final Resources res = getResources();
-        final ContentResolver resolver = getActivity().getContentResolver();
+		ContentResolver resolver = getActivity().getContentResolver();
 		
         // volume key cursor control
         mVolumeKeyCursorControl = (ListPreference) findPreference(VOLUME_KEY_CURSOR_CONTROL);
@@ -89,77 +60,11 @@ import com.aosmp.settings.preferences.CustomSeekBarPreference;
             mVolumeKeyCursorControl.setValue(Integer.toString(volumeRockerCursorControl));
            mVolumeKeyCursorControl.setSummary(mVolumeKeyCursorControl.getEntry());
         }
-
-        final boolean needsNavbar = ActionUtils.hasNavbarByDefault(getActivity());
-        final PreferenceCategory hwkeyCat = (PreferenceCategory) prefScreen
-                .findPreference(CATEGORY_HWKEY);
-        int keysDisabled = 0;
-        if (!needsNavbar) {
-            mHwKeyDisable = (SwitchPreference) findPreference(HWKEY_DISABLE);
-            keysDisabled = Settings.Secure.getIntForUser(getContentResolver(),
-                    Settings.Secure.HARDWARE_KEYS_DISABLE, 0,
-                    UserHandle.USER_CURRENT);
-            mHwKeyDisable.setChecked(keysDisabled != 0);
-            mHwKeyDisable.setOnPreferenceChangeListener(this);
-        } else {
-            prefScreen.removePreference(hwkeyCat);
-        }
-
-        // bits for hardware keys present on device
-        final int deviceKeys = getResources().getInteger(
-                com.android.internal.R.integer.config_deviceHardwareKeys);
-        // read bits for present hardware keys
-        final boolean hasHomeKey = (deviceKeys & KEY_MASK_HOME) != 0;
-        final boolean hasBackKey = (deviceKeys & KEY_MASK_BACK) != 0;
-        final boolean hasMenuKey = (deviceKeys & KEY_MASK_MENU) != 0;
-        final boolean hasAssistKey = (deviceKeys & KEY_MASK_ASSIST) != 0;
-        final boolean hasAppSwitchKey = (deviceKeys & KEY_MASK_APP_SWITCH) != 0;
-        // load categories and init/remove preferences based on device
-        // configuration
-        final PreferenceCategory backCategory =
-                (PreferenceCategory) prefScreen.findPreference(CATEGORY_BACK);
-        final PreferenceCategory homeCategory =
-                (PreferenceCategory) prefScreen.findPreference(CATEGORY_HOME);
-        final PreferenceCategory menuCategory =
-                (PreferenceCategory) prefScreen.findPreference(CATEGORY_MENU);
-        final PreferenceCategory assistCategory =
-                (PreferenceCategory) prefScreen.findPreference(CATEGORY_ASSIST);
-        final PreferenceCategory appSwitchCategory =
-                (PreferenceCategory) prefScreen.findPreference(CATEGORY_APPSWITCH);
-        // back key
-        if (!hasBackKey) {
-            prefScreen.removePreference(backCategory);
-        }
-        // home key
-        if (!hasHomeKey) {
-            prefScreen.removePreference(homeCategory);
-        }
-        // App switch key (recents)
-        if (!hasAppSwitchKey) {
-            prefScreen.removePreference(appSwitchCategory);
-        }
-        // menu key
-        if (!hasMenuKey) {
-            prefScreen.removePreference(menuCategory);
-        }
-        // search/assist key
-        if (!hasAssistKey) {
-            prefScreen.removePreference(assistCategory);
-        }
-        // let super know we can load ActionPreferences
-        onPreferenceScreenLoaded(ActionConstants.getDefaults(ActionConstants.HWKEYS));
-        // load preferences first
-        setActionPreferencesEnabled(keysDisabled == 0);        
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mHwKeyDisable) {
-            boolean value = (Boolean) objValue;
-            Settings.Secure.putInt(getContentResolver(), Settings.Secure.HARDWARE_KEYS_DISABLE,
-                    value ? 1 : 0);
-            setActionPreferencesEnabled(!value);
-		} else if (preference == mVolumeKeyCursorControl) {
+        if (preference == mVolumeKeyCursorControl) {
             String volumeKeyCursorControl = (String) objValue;
             int volumeKeyCursorControlValue = Integer.parseInt(volumeKeyCursorControl);
             Settings.System.putInt(getActivity().getContentResolver(),
@@ -169,17 +74,10 @@ import com.aosmp.settings.preferences.CustomSeekBarPreference;
             mVolumeKeyCursorControl
                     .setSummary(mVolumeKeyCursorControl.getEntries()[volumeKeyCursorControlIndex]);
             return true;                   
-        } else {
-			return false;
 		}
-		return true;
-	}
-
-    @Override
-    protected boolean usesExtendedActionsList() {
-        return true;
+        return false;
     }
-	
+
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.AOSMP;
